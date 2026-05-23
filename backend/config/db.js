68235e6b54abb33da328c351f28339321f0d886b
netsuwork <81774632+netsuwork@users.noreply.github.com>
2026-05-23 05:35:46 -0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,3 @@
 PORT=5000
-MONGO_URI=
-FOOTBALL_API_KEY=
+MONGO_URI=mongodb+srv://goaliqadmin:GOk7G8ypOYfKeV36@goaliq.kjulukd.mongodb.net/?appName=goaliq
+FOOTBALL_API_KEY=8fa33303e698461c9e7a78062121a9ad
