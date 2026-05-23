fad8eb97d19d7ac3abed5353802f7359a5dbd063
netsuwork <81774632+netsuwork@users.noreply.github.com>
2026-05-23 05:44:25 -0700

    Delete api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const matchController = require('../controllers/matchController');
-
-router.get('/:id', matchController.getMatch);
-
-module.exports = router;
