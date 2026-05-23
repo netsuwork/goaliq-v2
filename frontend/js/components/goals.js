6c8d3dce46562b591e2921ee8305f70a5043a4b2
netsuwork <81774632+netsuwork@users.noreply.github.com>
2026-05-23 05:28:40 -0700

    Update goals.js

--- frontend/js/components/goals.js
+++ frontend/js/components/goals.js
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GoalIQ Match</title>
 
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <div id="heroSection"></div>
+  <div id="goalsEl"></div>
+
+  <script src="js/utils/utils.js"></script>
+  <script src="js/services/api.js"></script>
+  <script src="js/components/hero.js"></script>
+  <script src="js/components/goals.js"></script>
+  <script src="js/pages/matchPage.js"></script>
+
+</body>
+</html>
